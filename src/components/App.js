6e7d1c1e0528ee1c17fae16f30fe9873cda8ff2a
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-07-17 17:41:32 +0900

    firebase, 로그인유지, 로그아웃
    - firebase 로그인
    - 계정 생성
    - 로그인 유지
    - 로그아웃

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
   //const auth = fbase.auth();
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   // 로그인상태가 false면 Auth, true면 Home이 보여짐
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing...."}
       <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
     </>
   );
